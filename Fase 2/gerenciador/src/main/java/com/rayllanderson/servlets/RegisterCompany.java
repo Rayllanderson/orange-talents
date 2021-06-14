3ebb125fb0c8de529b7fd564c5348d0d1e0218a9
Rayllanderson <rayllanderson@gmail.com>
2021-06-14 17:13:56 -0300

    enviando por post

--- Fase 2/gerenciador/src/main/java/com/rayllanderson/servlets/RegisterCompany.java
+++ Fase 2/gerenciador/src/main/java/com/rayllanderson/servlets/RegisterCompany.java
@@ -16,7 +16,7 @@ public class RegisterCompany extends HttpServlet {
 
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
-		String companyName = request.getParameter("name");
+		String companyName = request.getParameter("company-name");
 		out.println("<html> <body>");
 		out.println("Empresa " + companyName + " cadastrada com sucesso!");
 		out.println("</body> </html>");
