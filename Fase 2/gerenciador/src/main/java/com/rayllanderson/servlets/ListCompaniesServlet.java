7eb4045c28c6594515cd168072780b246992b6be
Rayllanderson <rayllanderson@gmail.com>
2021-06-15 09:47:59 -0300

    Carregando empresas via servlet & jsp

--- Fase 2/gerenciador/src/main/java/com/rayllanderson/servlets/ListCompaniesServlet.java
+++ Fase 2/gerenciador/src/main/java/com/rayllanderson/servlets/ListCompaniesServlet.java
@@ -1,7 +1,6 @@
 package com.rayllanderson.servlets;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -18,14 +17,8 @@ public class ListCompaniesServlet extends HttpServlet {
 	private CompanyService service = new CompanyService();
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		var html = new StringBuilder();
-		html.append("<html> <body>");
-		html.append("<h3>Empresas cadastradas: </h3>");
-		html.append("<table> <tr><th>Company Name</th></tr> ");
-		service.findAll().forEach(company -> html.append("<tr> <td>" + company.getName() + " </tr> </td>"));
-		html.append("</body> </html>");
-		PrintWriter out = response.getWriter();
-		out.println(html.toString());
+		request.setAttribute("companies", service.findAll());
+		request.getRequestDispatcher("/list-companies.jsp").forward(request, response);
 	}
 	
 }
