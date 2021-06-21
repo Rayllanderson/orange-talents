243ccf78304dbf7151cb6d6692685094338d676b
Rayllanderson <rayllanderson@gmail.com>
2021-06-21 11:14:52 -0300

    Filtrando

--- Fase 2/forum/src/main/java/com/rayllanderson/forum/controllers/TopicoController.java
+++ Fase 2/forum/src/main/java/com/rayllanderson/forum/controllers/TopicoController.java
@@ -7,6 +7,7 @@ import com.rayllanderson.forum.repositories.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
@@ -24,7 +25,11 @@ public class TopicoController {
     }
 
     @GetMapping
-    public List<TopicoDto> findAll(){
-        return TopicoDto.toTopicoDto(topicoRepository.findAll());
+    public List<TopicoDto> findAll(String nomeCurso){
+        if (nomeCurso == null){
+            return TopicoDto.toTopicoDto(topicoRepository.findAll());
+        } else {
+            return TopicoDto.toTopicoDto(topicoRepository.findByCursoNome(nomeCurso));
+        }
     }
 }
--- Fase 2/forum/src/main/java/com/rayllanderson/forum/repositories/TopicoRepository.java
+++ Fase 2/forum/src/main/java/com/rayllanderson/forum/repositories/TopicoRepository.java
@@ -3,5 +3,9 @@ package com.rayllanderson.forum.repositories;
 import com.rayllanderson.forum.entities.Topico;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
+    List<Topico> findByTitulo(String titulo);
+    List<Topico> findByCursoNome(String nome);
 }
