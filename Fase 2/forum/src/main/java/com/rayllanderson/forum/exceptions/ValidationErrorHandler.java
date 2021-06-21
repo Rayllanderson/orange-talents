e1fa7649b952c924e9404a369325c5d2fc502b19
Rayllanderson <rayllanderson@gmail.com>
2021-06-21 14:07:54 -0300

    Exception handler

---
+++ Fase 2/forum/src/main/java/com/rayllanderson/forum/exceptions/ValidationErrorHandler.java
@@ -0,0 +1,35 @@
+package com.rayllanderson.forum.exceptions;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class ValidationErrorHandler {
+
+    private final MessageSource messageSource;
+
+    @Autowired
+    public ValidationErrorHandler(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
+    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public List<ErroDeFormularioDto> handle (MethodArgumentNotValidException e) {
+        List<FieldError> fieldErrors = e.getBindingResult().getFieldErrors();
+        return fieldErrors.stream().map(erro -> {
+            String errorMessage = messageSource.getMessage(erro, LocaleContextHolder.getLocale());
+            return new ErroDeFormularioDto(erro.getField(), errorMessage);
+        }).collect(Collectors.toList());
+    }
+}
