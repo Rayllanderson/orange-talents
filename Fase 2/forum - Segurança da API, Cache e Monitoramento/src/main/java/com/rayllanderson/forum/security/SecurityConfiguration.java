1b8c9091d4202c6f36b0783cd1c3684bcc178cc3
Rayllanderson <rayllanderson@gmail.com>
2021-06-21 17:29:09 -0300

    Liberando acesso aos endpoints públicos

--- Fase 2/forum - Segurança da API, Cache e Monitoramento/src/main/java/com/rayllanderson/forum/security/SecurityConfiguration.java
+++ Fase 2/forum - Segurança da API, Cache e Monitoramento/src/main/java/com/rayllanderson/forum/security/SecurityConfiguration.java
@@ -1,9 +1,30 @@
 package com.rayllanderson.forum.security;
 
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    //Trata as configurações de Autenticação
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    }
+
+    //Trata as configurações de Autorização
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/topicos").permitAll()
+                .antMatchers(HttpMethod.GET, "/topicos/*").permitAll();
+    }
+
+    //Trata as configurações de recursos estáticos (js, css, imgs)
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+    }
 }
