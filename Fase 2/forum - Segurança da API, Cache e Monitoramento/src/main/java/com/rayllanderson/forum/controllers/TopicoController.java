ee0099f2c7af42bcd87f42595a131af6b01ba1d4
Rayllanderson <rayllanderson@gmail.com>
2021-06-21 17:04:44 -0300

    Adicionando Cache na aplicação

--- Fase 2/forum - Segurança da API, Cache e Monitoramento/src/main/java/com/rayllanderson/forum/controllers/TopicoController.java
+++ Fase 2/forum - Segurança da API, Cache e Monitoramento/src/main/java/com/rayllanderson/forum/controllers/TopicoController.java
@@ -9,6 +9,8 @@ import com.rayllanderson.forum.exceptions.NaoEncontradoException;
 import com.rayllanderson.forum.repositories.CursoRepository;
 import com.rayllanderson.forum.repositories.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
@@ -33,6 +35,7 @@ public class TopicoController {
     }
 
     @GetMapping
+    @Cacheable(value = "listaDeTopicos")
     public ResponseEntity<Page<TopicoDto>> findAll(@RequestParam(required = false) String nomeCurso, Pageable pageable) {
         if (nomeCurso == null){
             return ResponseEntity.ok(TopicoDto.toTopicoDto(topicoRepository.findAll(pageable)));
@@ -43,6 +46,7 @@ public class TopicoController {
 
     @Transactional
     @PostMapping
+    @CacheEvict(value = "listaDeTopicos", allEntries = true) // limpar todos os registros
     public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder) {
         Topico topico = form.toModel(cursoRepository);
         topico = topicoRepository.save(topico);
@@ -58,6 +62,7 @@ public class TopicoController {
 
     @Transactional
     @PutMapping("/{id}")
+    @CacheEvict(value = "listaDeTopicos", allEntries = true)
     ResponseEntity<TopicoDto> atualizar (@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm form){
         Topico topico = topicoRepository.findById(id).orElseThrow(() -> new NaoEncontradoException("Não encontrado"));
         Topico topicoAtualizado = form.atualizar(id, topico);
@@ -66,6 +71,7 @@ public class TopicoController {
 
     @Transactional
     @DeleteMapping("/{id}")
+    @CacheEvict(value = "listaDeTopicos", allEntries = true)
     public ResponseEntity<Void> remover(@PathVariable Long id) {
         topicoRepository.findById(id).orElseThrow(() -> new NaoEncontradoException("Não encontrado"));
         topicoRepository.deleteById(id);
