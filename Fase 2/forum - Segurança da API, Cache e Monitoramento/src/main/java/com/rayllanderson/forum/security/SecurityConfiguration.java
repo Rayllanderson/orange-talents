c8100857f71864c2b6b118f7e6eae070dddc7426
Rayllanderson <rayllanderson@gmail.com>
2021-06-22 09:10:20 -0300

    Restringindo o acesso aos endpoints privados

--- Fase 2/forum - Segurança da API, Cache e Monitoramento/src/main/java/com/rayllanderson/forum/security/SecurityConfiguration.java
+++ Fase 2/forum - Segurança da API, Cache e Monitoramento/src/main/java/com/rayllanderson/forum/security/SecurityConfiguration.java
@@ -20,7 +20,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers(HttpMethod.GET, "/topicos").permitAll()
-                .antMatchers(HttpMethod.GET, "/topicos/*").permitAll();
+                .antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
+                .anyRequest().authenticated()
+                .and().formLogin();
+
     }
 
     //Trata as configurações de recursos estáticos (js, css, imgs)
