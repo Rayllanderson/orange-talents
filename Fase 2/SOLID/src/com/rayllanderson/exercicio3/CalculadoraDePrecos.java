f000d8cc46234dee529587e72026e2a0e29ff8cf
Rayllanderson <rayllanderson@gmail.com>
2021-06-23 19:38:03 -0300

    Exercicio 3 - Não fatorado

---
+++ Fase 2/SOLID/src/com/rayllanderson/exercicio3/CalculadoraDePrecos.java
@@ -0,0 +1,14 @@
+package com.rayllanderson.exercicio3;
+
+public class CalculadoraDePrecos {
+
+    public double calcula(Compra produto) {
+        TabelaDePrecoPadrao tabela = new TabelaDePrecoPadrao();
+        Frete correios = new Frete();
+
+        double desconto = tabela.descontoPara(produto.getValor());
+        double frete = correios.para(produto.getCidade());
+
+        return produto.getValor() * (1-desconto) + frete;
+    }
+}
---
+++ Fase 2/SOLID/src/com/rayllanderson/exercicio3/Compra.java
@@ -0,0 +1,12 @@
+package com.rayllanderson.exercicio3;
+
+public class Compra {
+    private double valor;
+    private String cidade;
+    public double getValor() {
+        return valor;
+    }
+    public String getCidade() {
+        return cidade;
+    }
+}
