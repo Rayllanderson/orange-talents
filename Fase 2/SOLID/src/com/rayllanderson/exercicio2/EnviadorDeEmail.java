865493d98c5786643782faf75cabde9113b3a82c
Rayllanderson <rayllanderson@gmail.com>
2021-06-23 17:17:20 -0300

    Exercicio 2 - Não fatorado

---
+++ Fase 2/SOLID/src/com/rayllanderson/exercicio2/EnviadorDeEmail.java
@@ -0,0 +1,8 @@
+package com.rayllanderson.exercicio2;
+
+public class EnviadorDeEmail {
+
+    public void enviaEmail(NotaFiscal nf) {
+        System.out.println("envia email da nf " + nf.getId());
+    }
+}
---
+++ Fase 2/SOLID/src/com/rayllanderson/exercicio2/NotaFiscalDao.java
@@ -0,0 +1,7 @@
+package com.rayllanderson.exercicio2;
+
+public class NotaFiscalDao {
+    public void persiste(NotaFiscal nf) {
+        System.out.println("salva nf no banco");
+    }
+}
