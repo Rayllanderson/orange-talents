46d7354f5e7dc4d18f10cd9a1c87fd170d8ddf94
Rayllanderson <rayllanderson@gmail.com>
2021-07-27 14:09:35 -0300

    Gasto total com Fold

--- Fase 5/Kotlin/colecoes/src/main/kotlin/com/rayllanderson/colecoes/main4.kt
+++ Fase 5/Kotlin/colecoes/src/main/kotlin/com/rayllanderson/colecoes/main4.kt
@@ -8,7 +8,14 @@ fun main() {
 
     println("Salários base ${salarios.contentToString()}")
     println("Salários com aumento ${salariosComAumento.contentToString()}")
+    println("Soma de todos os salários este mês R$ ${salariosComAumento.soma()}")
+    println("Soma de todos os salários nos próximos 6 meses R$ ${salariosComAumento.getGastoTotalApos(6)}")
+}
 
+fun Array<BigDecimal>.getGastoTotalApos(meses: Int): BigDecimal{
+    return this.fold(0.toBigDecimal()){somador, salario ->
+        somador + (salario * meses.toBigDecimal())
+    }
 }
 
 fun bigDecimalArrayOf(vararg elementos: String): Array<BigDecimal> {
@@ -18,4 +25,10 @@ fun bigDecimalArrayOf(vararg elementos: String): Array<BigDecimal> {
 fun darAumento(salario: BigDecimal): BigDecimal{
     val aumento = "1.1".toBigDecimal()
     return if (salario < "5000.0".toBigDecimal()) salario + "500.0".toBigDecimal() else salario * aumento
+}
+
+fun Array<BigDecimal>.soma(): BigDecimal{
+    return this.reduce{acumulador, valor ->
+        acumulador + valor
+    }
 }
